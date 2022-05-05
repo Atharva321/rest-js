d8eae03e82aa7b3c13df7452715e0d6db02fabdb
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-05-05 23:32:17 +0530

    Update user.js

--- user.js
+++ user.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
+
 const dummyData = [{
     "name": "User1",
     "age": 9,
     "description": "User1"
   }
 ];
+
 router.get("/", (req, res) => {
   res.json(dummyData);
 });
