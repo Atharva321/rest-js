787e46c0bc29b850f77f0d4489340a52d5504889
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-05-05 23:20:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
+const userRouter = require('./routes/users');
+app.get('/', (req, res) => {
+  res.send("API is working");
+})
+app.use('/users', userRouter);
 const port = process.env.PORT || 5000;
 app.listen(port, () => `Server running on port ${port}.`);
